9599afed11f15766e9cafe08ce1aebc3013a0d61
we6jbo <we6jbo@gmail.com>
2014-12-29 19:46:18 -0800

    Create large_tile.php

---
+++ large_tile.php
@@ -0,0 +1,42 @@
+<html>
+<body>
+<?php
+echo "<div>\n";
+echo "    Unofficial HOP\n";
+echo "</div>\n";
+echo "<div>\n";
+echo "    <br/>\n";
+echo "</div>\n";
+echo "<div>\n";
+echo "    !! THE AUTHOR OF THIS INFUSION IS NOT RELATED TO THE DEVELOPER OF HOP.PHP !!\n";
+echo "</div>\n";
+if ((time() < strtotime("01/15/2015")) || (time() > strtotime("03/27/2015")) && (time() < strtotime("04/05/2015")) !== true)
+{
+ echo "<div>\n";
+ echo "    I do not plan to make any updates to this infusion in the near future. The sourcecode for this project can be found at\n";
+ echo "    https://github.com/we6jbo/unofficialhop/#. Feel free to use the code for your own project or expand on this Infusion by forking it.\n";
+ echo "</div>\n";
+}
+echo "<div>\n";
+echo "  <br/>\n";
+echo "</div>\n";
+echo "<div>\n";
+echo "    This infusion automates downloading and copying hop.php to the public directory on the pineapple at /www. If you do not know what hop.php is, there is a\n";
+echo "    video at https://www.youtube.com/watch?v=PZED4vR2uQ8 that will explain what hop.php is. In addition Mubix says at http://youtu.be/PZED4vR2uQ8?t=1m13s that \n";
+echo "    we have prepped thhis by putting hop.php on this specific pineapple. In the future additions of the pineapple it may be there by default, therefore this\n";
+echo "    Infusion should only be chosidered a temporary solution until hop.php is officially added to the pineapple.\n";
+echo "</div>\n";
+echo "<div>\n";
+echo "    <br/>\n";
+echo "</div>\n";
+echo "    About the author of Unofficial HOP:\n";
+echo "</div>\n";
+echo "<div>\n";
+echo "    <br/>\n";
+echo "    I go by the handle we6jbo. I was originally interested in the pineapple after watching Mubixs video on Youtube and how he used the Pineapple to create a\n";
+echo "    relay between systems. After purchasing the pineapple on the 52nd week I found out that hop.php did not come with the pineapple so I wrote this code to\n";
+echo "    install hop.php in the public www directory.\n";
+echo"</div>\n";
+?>
+</body>
+</html>
